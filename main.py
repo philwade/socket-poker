ccbad9d4ebe74139f3bf62db28f607f7d64e6a19
Philip Wade <phil@philwade.org>
2016-05-15 13:09:16 -0400

    Use the actual socket library

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'get sauced bruh'
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    socketio.run(app)
