99f4afc15dc906d31fc0f621f1a04a10ecd948b4
Philip Wade <phil@philwade.org>
2016-05-15 15:40:58 -0400

    Add session endpoint

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'get sauced bruh'
 socketio = SocketIO(app)
 
-@app.route('/')
+@app.route('/session/<string: token>')
 def index():
     return render_template('index.html')
+
+@app.route('/join')
+def join():
+    return render_template('join.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == '__main__':
     socketio.run(app)
