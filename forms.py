fa2a1d2602d0b1a53b9d36bca4829f088f3ce038
Phil Wade <phil@whoisstudio.com>
2016-06-02 07:30:43 -0400

    Rip out everything we don't need anymore.

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class CreateForm(Form):
-    sessionName = StringField('Session Name', validators=[DataRequired(message="required")])
-    username = StringField('Username', validators=[DataRequired(message="required")])
-    submit = SubmitField('Create Session')
-
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, session
 from flask_socketio import SocketIO
-from forms import CreateForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'get sauced bruh'
@@ -10,25 +9,6 @@ socketio = SocketIO(app)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/session/<token>')
-def session(token):
-    return render_template('session.html')
-
-@app.route('/join')
-def join():
-    return render_template('join.html')
-
-@app.route('/create', methods=['GET', 'POST'])
-def create():
-    form = CreateForm()
-
-    if form.validate_on_submit():
-        session['sessionName'] = form.sessionName.data
-        session['username'] = form.username.data
-        return redirect(url_for('session', form.sessionName.data))
-
-    return render_template('create.html', form=form)
 
 if __name__ == '__main__':
     socketio.run(app)
