69374ad6be52d8aa5b5254094a7a69c27c848b92
Phil Wade <phil@whoisstudio.com>
2016-06-10 11:16:41 -0400

    Clean up layout, get rid of components

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,20 +1,12 @@
 import { h, Component } from 'preact';
 import style from './style';
-import Chat from './chat';
+import IssueCard from './issueCard';
 import VotingUsers from '../../containers/votingUsers';
-import IssueList from './issueList';
 import VotingPanelContainer from '../../containers/votingPanelContainer';
 
 let state = {
 	currentUser: 'guy',
-	messages: [
-		{ id: 1, author: 'guy', content: 'hello' },
-		{ id: 0, author: 'someoneelse', content: 'whattaup' }
-	],
-	issues: [
-		{ id: 1, title: 'Add Socket Support', description: 'Update the UI via socket responses', score: '' },
-		{ id: 0, title: 'Custom Votes', description: 'Allow custom voting numbers', score: '' }
-	]
+	currentIssue: { title: 'Add Socket Support', description: 'Update the UI via socket responses', score: '' }
 };
 
 export default class Home extends Component {
@@ -22,11 +14,8 @@ export default class Home extends Component {
 		return (
 			<div className="cyan darken-1">
 				<div className="row">
-					<div className="col s6">
-						<IssueList issues={state.issues} />
-					</div>
-					<div className="col s6">
-						<Chat messages={state.messages} />
+					<div className="col s8 offset-s2">
+						<IssueCard issue={state.currentIssue} />
 						<VotingUsers />
 						<VotingPanelContainer />
 					</div>
