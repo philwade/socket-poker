5af83114cbcb0eedf07a2429625601a1f02dfa40
Phil Wade <phil@whoisstudio.com>
2016-06-13 14:06:05 -0400

    Some massaging styles and UI

--- frontend/src/components/home/userList.js
+++ frontend/src/components/home/userList.js
@@ -3,15 +3,19 @@ import { h, Component } from 'preact';
 class UserList extends Component {
 	render({ users, votesVisible }) {
 		let displayUsers = users.map((user) => {
+			let voteStatus = votesVisible ? (user.vote ? user.vote : '?') : <i class="material-icons white-text">{ user.vote ? 'check' : 'timelapse'}</i>
 			return (
-			<div key={user.id} className="user">
-				<strong>{ user.vote ? (votesVisible ? user.vote : 'X') : '?'}</strong> { user.name }
+			<div key={user.id} class="collection-item cyan darken-3">
+				  <span class='userVote'>{voteStatus}</span> { user.name }
 			</div>
 			);
 		});
 
 		return (
-			<div className="userList">
+			<div class="userlist collection with-header white-text cyan darken-3">
+				<div class="collection-header cyan darken-3">
+					<h5>Your team</h5>
+				</div>
 				{ displayUsers }
 			</div>
 		);
