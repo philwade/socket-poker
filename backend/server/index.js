aaa309138f560f882d8cdbb4c052caf3fd025a9e
Phil Wade <phil@whoisstudio.com>
2016-06-14 16:55:59 -0400

    Attempt to pull stuff out of mongo

--- backend/server/index.js
+++ backend/server/index.js
@@ -4,7 +4,9 @@ import { createStore } from 'redux';
 import pokerApp from '../reducers';
 import { toggle_vote_visibility } from '../actions';
 
-var app = express();
+import monk from 'monk';
+
+let app = express();
 app.server = http.createServer(app);
 
 let store = createStore(pokerApp);
@@ -13,11 +15,26 @@ store.subscribe(() => {
 	console.log(store.getState());
 });
 
+let db = monk('localhost:27017/poker');
+let sessions = db.get('sessions');
+//sessions.insert(store.getState());
 store.dispatch(toggle_vote_visibility());
 
-app.get('/', (req, res) => {
-	res.send('<h1>eyo whaatup</h1>');
+app.get('/api/session/:id', (req, res) => {
+	let id = req.params.id;
+	sessions.findById(id, (err, doc) => {
+		if(!err) {
+			res.send('errrr ' + err);
+			return;
+		}
+		res.send(doc);
+	});
 });
+
+app.post('/api/session', (req, res) => {
+	res.send('create session');
+});
+
 
 app.server.listen(process.env.PORT || 8080);
 console.log(`Started on port ${app.server.address().port}`);
