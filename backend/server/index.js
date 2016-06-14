83ede87635b61f3a2da4284e06a44b1a84edb30c
Phil Wade <phil@whoisstudio.com>
2016-06-14 15:14:12 -0400

    Basic action/store hookup for the server side.

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,8 +1,19 @@
 import http from 'http';
 import express from 'express';
+import { createStore } from 'redux';
+import pokerApp from '../reducers';
+import { toggle_vote_visibility } from '../actions';
 
 var app = express();
 app.server = http.createServer(app);
+
+let store = createStore(pokerApp);
+
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+store.dispatch(toggle_vote_visibility());
 
 app.get('/', (req, res) => {
 	res.send('<h1>eyo whaatup</h1>');
