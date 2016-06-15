b5b5709ad62e4543c0b675b4a0b011fd4ce5cd8d
Philip Wade <phil@philwade.org>
2016-06-15 18:25:23 -0400

    Make redirect to session button work sort of.

--- frontend/src/components/landing/index.js
+++ frontend/src/components/landing/index.js
@@ -1,7 +1,21 @@
 import { h, Component } from 'preact';
+import { route } from 'preact-router';
 
 export default class Landing extends Component {
-	render() {
+	constructor() {
+		super();
+		this.state.session = '';
+	}
+
+	redirectToVoting() {
+		route('session/' + this.state.session);
+	}
+
+	onChange(e) {
+		this.setState({ session: e.target.value});
+	}
+
+	render(props, state) {
 		return (
 			<div class="row">
 				<div class="col s8 offset-s2">
@@ -10,7 +24,16 @@ export default class Landing extends Component {
 							<h3>Welcome to Socket Poker</h3>
 							<p>To get started create a new session or join one.</p>
 							<p><a class="waves-effect waves-light btn"><i class="material-icons left">play_arrow</i>Start a new session</a></p>
-							<p><a class="waves-effect waves-light btn"><i class="material-icons left">play_for_work</i>Join a session</a><input type="text" placeholder="Enter a session Id"/></p>
+							<p>
+								<a class="waves-effect waves-light btn"
+									onClick={() => this.redirectToVoting()}>
+									<i class="material-icons left">play_for_work</i>
+									Join a session
+								</a>
+								<input type="text" value={state.session}
+									onChange={(e) => this.onChange(e)}
+									placeholder="Enter a session Id"/>
+							</p>
 						</div>
 					</div>
 				</div>
