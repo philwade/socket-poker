78728ca711ab3a4213e018295491b33e57711482
Phil Wade <phil@whoisstudio.com>
2017-02-28 09:15:38 -0500

    Build for backend, add lib symlink, add fetch to backend

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,2 +1,83 @@
-export * from './pure';
-export * from './async';
+import 'isomorphic-fetch';
+import guid from '../lib/guid';
+
+export const VOTE = 'VOTE';
+export const UPDATE_ISSUE = 'UPDATE_ISSUE';
+export const TOGGLE_VOTE_VISIBILITY = 'TOGGLE_VOTE_VISIBLITY';
+export const CLEAR_VOTES = 'CLEAR_VOTES';
+export const ADD_USER = 'ADD_USER';
+// Set who we are
+export const SET_USER = 'SET_USER';
+// receive state from the server
+export const HYDRATE_STATE = 'HYDRATE_STATE';
+
+export const vote = (userId, value) => {
+	return {
+		type: VOTE,
+		userId,
+		vote: value
+	};
+};
+
+export const update_issue = (title, content) => {
+	return {
+		type: UPDATE_ISSUE,
+		title,
+		content
+	};
+};
+
+export const toggle_vote_visibility = () => {
+	return {
+		type: TOGGLE_VOTE_VISIBILITY
+	};
+};
+
+export const clear_votes = () => {
+	return {
+		type: CLEAR_VOTES
+	};
+};
+
+export const add_user = (name, id) => {
+	return {
+		type: ADD_USER,
+		name,
+		userId: id
+	};
+};
+
+export const hydrate_state = (state) => {
+	return {
+		local: true,
+		type: HYDRATE_STATE,
+		state
+	};
+};
+
+export const set_user = (username, id) => {
+	return {
+		local: true,
+		type: SET_USER,
+		user: {
+			id,
+			name: username
+		}
+	};
+};
+
+export const create_session = () => {
+	return (dispatch) => {
+		return fetch('/api/session', {method:'POST'})
+			.then(response => response.json())
+			.then(json => dispatch(hydrate_state(json)));
+	};
+};
+
+export const fetch_session = (id) => {
+	return (dispatch) => {
+		return fetch('/api/session/' + id)
+			.then(response => response.json())
+			.then(json => dispatch(hydrate_state(json)));
+	};
+};
