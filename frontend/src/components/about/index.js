e0021ba9bfea062f919b275a370a7ca6ce6be385
Phil Wade <phil@whoisstudio.com>
2017-04-14 10:22:02 -0400

    Edits

--- frontend/src/components/about/index.js
+++ frontend/src/components/about/index.js
@@ -7,9 +7,10 @@ const About = () => (
 				<div class="card-content">
 					<h3>About Socket Poker</h3>
 					<p>I built Socket Poker to learn about preact, redux and websockets. I ended up learning about node and express too. If you'd like to get in touch, my email address is phil at philwade dot org. I'm also on twitter <a href="https://twitter.com/phil_wade">@phil_wade</a></p>
-					<p>It is also open source software, on github here: <a href="https://github.com/philwade/socket-poker">https://github.com/philwade/socket-poker</a>
+					<br></br>
+					<p>Socket Poker is open source software, on github here: <a href="https://github.com/philwade/socket-poker">https://github.com/philwade/socket-poker</a></p>
 					<h3>About Pointing Poker</h3>
-					<p>Pointing poker is a way for agile teams to size tickets for inclusion in sprints. For more, look at <a href="https://en.wikipedia.org/wiki/Planning_poker">https://en.wikipedia.org/wiki/Planning_poker</a></p>
+					<p>Pointing poker is a way for agile teams to size tickets for inclusion in sprints. For more: <a href="https://en.wikipedia.org/wiki/Planning_poker">https://en.wikipedia.org/wiki/Planning_poker</a></p>
 				</div>
 			</div>
 		</div>
